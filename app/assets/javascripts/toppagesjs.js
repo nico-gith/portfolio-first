46e32c46a68279211151bf17c6155cd0207c09da
kai <deepdish.mind@gmail.com>
2021-05-25 12:52:12 +0000

    first commit

---
+++ app/assets/javascripts/toppagesjs.js
@@ -0,0 +1,34 @@
+/* global $*/
+$(function () {
+  //ページ内スクロール
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, 300, "swing");
+    return false;
+  });
+  
+  //ナビバースクロール時固定
+  //スクロールすると上部に固定させるための設定を関数でまとめる
+  function FixedAnime() {
+	var headerH = $('.header').outerHeight(true);
+	var scroll = $(window).scrollTop();
+	if (scroll >= headerH){//headerの高さ以上になったら
+			$('.hnav').addClass('fixed');
+		}else{
+			$('.hnav').removeClass('fixed')
+		}
+  }
+
+  // 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function () {
+	  FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+  });
+
+  // ページが読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function () {
+	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+  });
+
+});
